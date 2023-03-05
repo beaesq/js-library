587981b2c963561091db5694e8cadd7ef5aba7c4
Bwa <40144023+beaesq@users.noreply.github.com>
2023-03-06 00:48:25 +0800

    Add new book form content

--- library.js
+++ library.js
@@ -43,14 +43,28 @@ function displayBooks() {
   }
 }
 
+// new book modal
+const modal = document.getElementById('new-book-modal');
+const modalBtn = document.getElementById('btn-new-book');
+const modalClose = document.getElementsByClassName('close')[0];
+
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+modalClose.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
 addBookToLibrary('1984', 'George Orwell', 328, true);
 addBookToLibrary('The Time Machine', 'H. G. Wells', 84, false);
 addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 42, false);
-addBookToLibrary('1984', 'George Orwell', 328, true);
-addBookToLibrary('The Time Machine', 'H. G. Wells', 84, false);
-addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 42, false);
-addBookToLibrary('1984', 'George Orwell', 328, true);
-addBookToLibrary('The Time Machine', 'H. G. Wells', 84, false);
-addBookToLibrary('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 42, false);
+
 
 displayBooks();
